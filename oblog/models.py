6e6123f3e64b473e32504b2629962b0330fa4b39
Omar Elloumi <omarelloumi842@yahoo.fr>
2025-08-15 01:57:30 +0200

    create customUser model

--- oblog/models.py
+++ oblog/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+
+class CustomUser(AbstractUser):
+    bio = models.TextField(null=True, blank=True)
+    profile_picture = models.ImageField(upload_to='profile_img', null=True, blank=True)
+    facebook = models.URLField(null=True, blank=True)
+    twitter = models.URLField(null=True, blank=True)
+    instagram = models.URLField(null=True, blank=True)
+    youtube = models.URLField(null=True, blank=True)
+
+    def __str__(self):
+        return self.username
