631ef49c5fa21e2a6d6d3ea3f5090a18bfbe59ac
Omar Elloumi <omarelloumi842@yahoo.fr>
2025-08-15 02:53:48 +0200

    add user registration

--- oblog/views.py
+++ oblog/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.status import HTTP_400_BAD_REQUEST
+from oblog.serializers import UserRegisterSerializer
 
 # Create your views here.
+
+@api_view(['POST'])
+def register_user(request):
+    serializer = UserRegisterSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors,status=HTTP_400_BAD_REQUEST)
